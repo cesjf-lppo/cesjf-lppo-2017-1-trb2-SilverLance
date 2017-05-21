d5eb1f50a41b878faf02b495cca63ceadaab2ed7
AdrianoXavier <aj-dx@hotmail.com>
2017-05-21 11:01:43 -0300

    Criando classe Coleta.

---
+++ src/java/br/cesjf/lppo/classes/Coleta.java
@@ -0,0 +1,59 @@
+package br.cesjf.lppo.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Adriano
+ */
+public class Coleta {
+   private int id;
+   private String descricao;
+   private Date data;
+
+   public Coleta(){
+       
+   }
+   
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
